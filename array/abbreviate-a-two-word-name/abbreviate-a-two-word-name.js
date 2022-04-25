021dc81e46e76fe799320e9faa9573b00de750c7
vee <iam.vee01@gmail.com>
2022-04-25 20:30:54 +0100

    Create a function to abbreviate names and return the initials

--- array/abbreviate-a-two-word-name/abbreviate-a-two-word-name.js
+++ array/abbreviate-a-two-word-name/abbreviate-a-two-word-name.js
@@ -1,1 +1,20 @@
+/*
+  Write a function to convert a name into initials. The input strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+*/
+
+const convertToInitials = (fullName) => {
+  fullName = fullName.split( ' ' )
+  
+//[0][0]- get the first letter of the firstname, [1][0]- get the first letter of the second name
+  return `${fullName[0][0]}.${fullName[1][0]}`.toUpperCase()
+
+}
+let output = convertToInitials('victor abolarin')
+console.log(`My Initials are ${output} `)
 
