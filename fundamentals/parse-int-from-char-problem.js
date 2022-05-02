b57d5623ee3979d19165d0d172124dc6ce738d86
iamvee01 <>
2022-05-02 04:39:00 +0000

    Write program to return random age in integer from given string

---
+++ fundamentals/parse-int-from-char-problem.js
@@ -0,0 +1,18 @@
+/*
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+*/
+
+const randomAge = Math.floor(Math.random() * 33)
+// console.log(randomAge)
+
+const getAge = (ageString) => {
+  let age = +( ageString[0] )
+  console.log(typeof age)
+  return age
+}
+
+console.log( getAge(`${randomAge} years old`))
