977f3a740eb6d8baa49e902a72904f4889334962
victor <iam.vee01@gmail.com>
2022-06-15 15:56:14 +0100

    Write a function that returns a given integer in descending order

---
+++ fundamentals/descending-order.js
@@ -0,0 +1,21 @@
+/*
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+*/
+
+const descendingOrder = (numb) => {
+
+    //split argument to array
+    let splitNumb = numb.split('')
+
+    //sort array in descending order
+   let sortSplitNumb = splitNumb.sort((a, b) => {
+        return (b - a)
+    })
+
+    return sortSplitNumb.join('')
+
+}
+console.log(descendingOrder('034251'))
