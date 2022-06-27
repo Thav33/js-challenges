f4557f5d26284257ebef0b9a14857f05b5a85d2f
victor <iam.vee01@gmail.com>
2022-06-27 01:21:49 +0100

    Write a function to play rps game

---
+++ fundamentals/rock-paper-scissors.js
@@ -0,0 +1,22 @@
+/*
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+*/
+
+const rps = (player1, player2) => {
+    let result = (player1 == 'scissors' && player2 == 'paper') ? 'Player 1 won' :
+    (player1 === 'paper' && player2 === 'rock') ? 'player 1 won' :
+    (player1 === 'rock' && player2 === 'scissors') ? 'Player 1 won' :
+    (player1 === player2) ? 'Draw' :
+    'Player 2 won'
+
+    return result;
+
+}
+
+console.log(rps('scissors', 'paper'))
