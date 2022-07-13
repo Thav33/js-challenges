f5ce164a91d8068a0e9268a78a0828d1edac4696
victor <iam.vee01@gmail.com>
2022-07-13 08:04:42 +0100

    Write function to convert array to string

---
+++ array/array-to-string.js
@@ -0,0 +1,15 @@
+/*
+Write a simple JavaScript program to join all elements of the following array into a string.
+
+Example: ["Red", "Green", "White", "Black"];
+
+Expected Output :
+"Red,Green,White,Black"
+*/
+
+const arrayToString = (arr) => {
+
+ return arr.join()
+    
+}
+console.log(arrayToString( ["Red", "Green", "White", "Black"] ))
